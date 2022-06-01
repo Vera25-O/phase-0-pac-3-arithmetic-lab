25d3a09b221d3caff06faf5b9bcaaebe69bd8bbe
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-01 22:48:59 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+var a, b
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a/b;
+  }
+  function increment(a) {
+    return a+1;
+  }
+  function decrement(a) {
+    return a-1;
+  }
+  function makeInt(a) {
+    return a;
+  }
+
+  function makeInt(sldkjflksjf){
+    return true;
+  }
+   
+   function makeInt(a=0x2328) {
+    return parseInt(a, 10)
+  }
+  function preserveDecimal(sldkjflksjf){
+    return true;
+  }
+  function preserveDecimal(n=2.222) {
+    return parseFloat(n);
+  }
+    
